c30ce5039732398f93e13c449a5adcc1b61f3217
Valentine Wanjiru <kt116tabitha@gmail.com>
2025-05-23 14:44:32 +0300

    feat:controlled form

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,35 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onAddItem }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchText, setSearchText] = useState("");
+
+  function handleFilteredItem(event) {
+    setSearchText(event.target.value)
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
 
-    return item.category === selectedCategory;
-  });
+  const itemsToDisplay = items.filter((item) => {
+  const categoryMatch = selectedCategory === "All" || item.category === selectedCategory;
+  const nameMatch = item.name.toLowerCase().includes(searchText.toLowerCase());
+  return categoryMatch && nameMatch;
+});
+
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onAddItem}/>
+      <Filter
+      onCategoryChange={handleCategoryChange}
+      onSearchChange={handleFilteredItem}
+      search={searchText}
+      selectedCategory={selectedCategory}
+       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
